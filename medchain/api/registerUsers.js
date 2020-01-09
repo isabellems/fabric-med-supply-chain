6c7166997c99588893caeea1031e85643216eef0
Maria-Isavella Manolaki-Sempagios <isabellenerd@gmail.com>
2020-01-09 23:44:39 +0200

    Added file for registering users based on parameters

---
+++ medchain/api/registerUsers.js
@@ -0,0 +1,18 @@
+const addUser = require('./lib/addUser');
+
+async function registerUsers() {
+   try {
+	//await addUser('peer0.org1', 'org1');
+	//await addUser('peer1.org1', 'org1');
+        await addUser('peer0.org2', 'org2');
+	//await addUser('peer1.org2', 'org2');
+	//await addUser('peer0.org3', 'org3');
+	//await addUser('peer1.org3', 'org3');
+   } catch(e) {
+	console.log(e);
+   }
+
+}
+
+
+registerUsers();
