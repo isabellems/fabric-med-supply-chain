29cd253d7425402f994801ffd2f33f402a695d9d
Maria-Isavella Manolaki-Sempagios <isabellenerd@gmail.com>
2020-01-19 23:15:54 +0200

    Added error handling in apiserver

--- medchain/api/apiserver.js
+++ medchain/api/apiserver.js
@@ -8,203 +8,202 @@ const path = require('path');
 var app = express();
 app.use(bodyParser.json());
 
-const ccpPath = path.resolve(__dirname, '..', '..', 'first-network', 'connection-org1.json');
-console.log(ccpPath)
+console.log('Server started! Listening to port 3000');
 
 app.use(authorizeUser, validateIdentity, createGateway);
 
 app.post('/api/createDrugPackage', async (req, res) => {
-  try {
-    
-    console.log('In route');
-    
-    const gateway = req.gateway;
-    const contract = req.contract;
-    console.log(req.body);
+  const gateway = req.gateway;
+  const contract = req.contract;
+  console.log(req.body);
 
-    let id = req.body.id;
-    let name = req.body.name;
-    let manufacturer = req.body.manufacturer;
-    let temperature = req.body.temperature;
-    let locationLatitude = req.body.locationLatitude;
-    let locationLongitude = req.body.locationLongitude;
-    let holder = req.body.holder;
-    let pieces = req.body.pieces;
-    let timestamp = new Date().getTime().toString();
+  let id = req.body.id;
+  let name = req.body.name;
+  let manufacturer = req.body.manufacturer;
+  let temperature = req.body.temperature;
+  let locationLatitude = req.body.locationLatitude;
+  let locationLongitude = req.body.locationLongitude;
+  let holder = req.body.holder;
+  let pieces = req.body.pieces;
+  let timestamp = new Date().getTime().toString();
 	
-    if(!id || !name || !manufacturer || !temperature || !locationLatitude || !locationLongitude || !holder || !pieces) {
-      console.log('Not sufficient arguments');
-      return res.status(200).send({ error: "Insufficient arguments" });
-    }
-
-    console.log('Before submit');
-    await contract.submitTransaction('initDrugPackage', id, name, manufacturer, temperature, locationLatitude, locationLongitude, holder, pieces, timestamp);
-    console.log('After submit');
-    res.sendStatus(200);
-
-    await gateway.disconnect();
-
-  } catch(e) {
-    console.error(`Failed to submit transaction: ${e}`);
-    return res.status(200).send({ error: e });
+  if(!id || !name || !manufacturer || !temperature || !locationLatitude || !locationLongitude || !holder || !pieces) {
+    console.log('Not sufficient arguments');
+    return res.status(200).send({ error: "Insufficient arguments" });
   }
+  console.log('Submitting transaction...');
+  
+  try {
+    await contract.submitTransaction('initDrugPackage', id, name, manufacturer, temperature, locationLatitude, locationLongitude, holder, pieces, timestamp);
+    console.log('Transaction submitted successfully');
+  } catch(e) {
+    console.log('Failed to submit transaction');
+    console.log(e);
+    res.status(200).send({ error: e.message });
+    await gateway.disconnect();
+    return;
+  }
+  res.sendStatus(200);
+  await gateway.disconnect();
 });
 
 app.put('/api/transferDrugPackage/:id', async (req, res) => {
-   try {
-
-    const gateway = req.gateway;
-    const contract = req.contract;
+  const gateway = req.gateway;
+  const contract = req.contract;
     
-    let id = req.params.id;
-    let temperature = req.body.temperature;
-    let locationLatitude = req.body.locationLatitude;
-    let locationLongitude = req.body.locationLongitude;
-    let holder = req.body.holder;
-    let pieces = req.body.pieces;
-    let timestamp = new Date().getTime().toString();
+  let id = req.params.id;
+  let temperature = req.body.temperature;
+  let locationLatitude = req.body.locationLatitude;
+  let locationLongitude = req.body.locationLongitude;
+  let holder = req.body.holder;
+  let pieces = req.body.pieces;
+  let timestamp = new Date().getTime().toString();
 	
-    if(!id || !temperature || !locationLatitude || !locationLongitude || !holder || !pieces) {
-      console.log('Not sufficient arguments');
-      return res.status(200).send({ error: "Insufficient arguments" });
-    }
-    
-    console.log('Here');
-    await contract.submitTransaction('transferDrugPackage', id, holder, temperature, locationLatitude, locationLongitude, pieces, timestamp);
-    console.log('There');
-    res.sendStatus(200);
-
-    await gateway.disconnect();
-
-  } catch(e) {
-    console.error(`Failed to submit transaction: ${e}`);
-    return res.status(200).send({ error: e });
+  if(!id || !temperature || !locationLatitude || !locationLongitude || !holder || !pieces) {
+    console.log('Not sufficient arguments');
+    return res.status(200).send({ error: "Insufficient arguments" });
   }
-
+    
+  console.log('Submitting transaction...');
+  try {
+    await contract.submitTransaction('transferDrugPackage', id, holder, temperature, locationLatitude, locationLongitude, pieces, timestamp);
+    console.log('Transaction submitted successfully');
+  } catch(e) {
+    console.log('Failed to submit transaction');
+    console.log(e);
+    res.status(200).send({ error: e.message });
+    await gateway.disconnect();
+    return;
+  }
+  res.sendStatus(200);
+  await gateway.disconnect();
 });
 
 app.put('/api/moveDrugPackage/:id', async (req, res) => {
-  try {
-
-    const gateway = req.gateway;
-    const contract = req.contract;
+  const gateway = req.gateway;
+  const contract = req.contract;
     
-    let id = req.params.id;
-    let temperature = req.body.temperature;
-    let locationLatitude = req.body.locationLatitude;
-    let locationLongitude = req.body.locationLongitude;
-    let timestamp = new Date().getTime().toString();
+  let id = req.params.id;
+  let temperature = req.body.temperature;
+  let locationLatitude = req.body.locationLatitude;
+  let locationLongitude = req.body.locationLongitude;
+  let timestamp = new Date().getTime().toString();
 	
-    if(!id || !temperature || !locationLatitude || !locationLongitude) {
-      console.log('Not sufficient arguments');
-      return res.status(200).send({ error: "Insufficient arguments" });
-    }
-
-    let result = await contract.submitTransaction('moveDrugPackage', id, temperature, locationLatitude, locationLongitude, timestamp);
-    res.sendStatus(200);
-
-    await gateway.disconnect();
-
-  } catch(e) {
-    console.error(`Failed to submit transaction: ${e}`);
-    return res.status(200).send({ error: e });
+  if(!id || !temperature || !locationLatitude || !locationLongitude) {
+    console.log('Not sufficient arguments');
+    return res.status(200).send({ error: "Insufficient arguments" });
   }
 
+  console.log('Submitting transaction...');
+  try {
+    await contract.submitTransaction('moveDrugPackage', id, temperature, locationLatitude, locationLongitude, timestamp);
+    console.log('Transaction submitted successfully');
+  } catch(e) {
+    console.log('Failed to submit transaction');
+    console.log(e)
+    res.status(200).send({ error: e.message });
+    await gateway.disconnect();
+    return;
+  }
+  res.sendStatus(200);
+  await gateway.disconnect();
 });
 
 app.get('/api/drugPackages/:id', async (req, res) => {
- try {
+  const gateway = req.gateway;
+  const contract = req.contract;
 
-    const gateway = req.gateway;
-    const contract = req.contract;
-
-    let id = req.params.id;
-	
-    if(!id) {
-      console.log('Not sufficient arguments');
-      return res.status(200).send({ error: "Insufficient arguments" });
-    }
-
-    let result = await contract.evaluateTransaction('readDrugPackage', id);
+  let id = req.params.id; 
+  if(!id) {
+    console.log('Not sufficient arguments');
+    return res.status(200).send({ error: "Insufficient arguments" });
+  }
+  let result;
+  console.log('Evaluating transaction...');
+  try {
+    result = await contract.evaluateTransaction('readDrugPackage', id);
     console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
-    res.status(200).json({ response: result.toString() });
-
-    await gateway.disconnect();
-
   } catch(e) {
     console.error(`Failed to submit transaction: ${e}`);
-    return res.status(200).send({ error: e });
+    res.status(200).send({ error: e.message });
+    await gateway.disconnect();
+    return;
   }
 
+  res.status(200).json({ response: result.toString() });
+  await gateway.disconnect();
 });
 
 app.post('/api/drugPackages', async (req, res) => {
- try {
 
-    const gateway = req.gateway;
-    const contract = req.contract;
+  const gateway = req.gateway;
+  const contract = req.contract;
     
-    let selector = {};
-    selector.name = req.body.name ? req.body.name : undefined
-    selector.manufacturer = req.body.manufacturer ? req.body.manufacturer : undefined
-    selector.temperature = req.body.temperature ? req.body.temperature : undefined
-    selector.holder = req.body.holder ? req.body.holder : undefined
-    selector.pieces = req.body.pieces ? req.body.pieces : undefined
-    selector.dateCreated = req.body.dateCreated ? req.body.dateCreated : undefined
-    selector.dateShipped = req.body.dateShipped ? req.body.dateShipped : undefined
-    if (req.body.location) {
-	    selector.location = {}
-	    if (req.body.location.latitude) {
-		    selector.location.latitude = req.body.location.latitude
-	    }
-	    if (req.body.location.longitude) {
-		    selector.location.longitude = req.body.location.longitude
-            }
+  let selector = {};
+  selector.name = req.body.name ? req.body.name : undefined
+  selector.manufacturer = req.body.manufacturer ? req.body.manufacturer : undefined
+  selector.temperature = req.body.temperature ? req.body.temperature : undefined
+  selector.holder = req.body.holder ? req.body.holder : undefined
+  selector.pieces = req.body.pieces ? req.body.pieces : undefined
+  selector.dateCreated = req.body.dateCreated ? req.body.dateCreated : undefined
+  selector.dateShipped = req.body.dateShipped ? req.body.dateShipped : undefined
+  if (req.body.location) {
+    selector.location = {}
+    if (req.body.location.latitude) {
+      selector.location.latitude = req.body.location.latitude
     }
-    console.log('selector')
-    console.log(selector)
-    let query = JSON.stringify({ selector })
-    console.log('query')
-    console.log(query)
-	
-    let result = await contract.evaluateTransaction('queryDrugPackages', query);
+    if (req.body.location.longitude) {
+      selector.location.longitude = req.body.location.longitude
+    }
+  }
+  console.log('Selector:');
+  console.log(selector)
+  let query = JSON.stringify({ selector })
+  console.log('Query:');
+  console.log(query);
+    
+  let result;
+  console.log('Evaluating transaction...');
+  try {
+    result = await contract.evaluateTransaction('queryDrugPackages', query);
     console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
-    res.status(200).json({ response: result.toString() });
-
-    await gateway.disconnect();
-
   } catch(e) {
     console.error(`Failed to submit transaction: ${e}`);
-    return res.status(200).send({ error: e });
+    res.status(200).send({ error: e.message });
+    await gateway.disconnect();
+    return;
   }
 
-
-})
+  res.status(200).json({ response: result.toString() });
+  await gateway.disconnect();
+});
 
 app.get('/api/history/:id', async (req, res) => {
- try {
 
-    const gateway = req.gateway;
-    const contract = req.contract;
+  const gateway = req.gateway;
+  const contract = req.contract;
     
-    let id = req.params.id;
-	
-    if(!id) {
-      console.log('Not sufficient arguments');
-      return res.status(200).send({ error: "Insufficient arguments" });
-    }
+  let id = req.params.id;
 
-    let result = await contract.evaluateTransaction('getHistoryForDrugPackage', id);
-    console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
-    res.status(200).json({ response: result.toString() });
-
-    await gateway.disconnect();
-
-  } catch(e) {
-    console.error(`Failed to submit transaction: ${e}`);
-    return res.status(200).send({ error: e });
+  if(!id) {
+    console.log('Not sufficient arguments');
+    return res.status(200).send({ error: "Insufficient arguments" });
   }
 
+  let result;
+  console.log('Evaluating transaction...');
+  try {
+    result = await contract.evaluateTransaction('getHistoryForDrugPackage', id);
+    console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+  } catch(e) {
+    console.error(`Failed to submit transaction: ${e}`);
+    res.status(200).send({ error: e });
+    await gateway.disconnect();
+    return;
+  }
+
+  res.status(200).json({ response: result.toString() });
+  await gateway.disconnect();
 });
 
 app.listen(3000);
